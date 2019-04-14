ca801aeb874d51f12df60f4801b7c3b6dca9d04a
wuchunlong0 <wcl6005@163.com>
2019-04-14 16:58:42 +0800

    account

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,31 +1,28 @@
-# -*- coding: UTF-8 -*-
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url,include
-from django.contrib.auth import views as auth_views 
+from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'login/$', auth_views.LoginView.as_view(), name='login'),  
-    url(r'logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    
-    #path(r'blog/', include('blog.urls')),
-    path(r'account/', include('account.urls')),
-    
-    url(r'^$', RedirectView.as_view(url='/account/billing_vue/')), #
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^(?P<path>assets(/.*))$', RedirectView.as_view(url='/static/%(path)s', query_string=True)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('account.urls')),
+    url(r'^$', RedirectView.as_view(url='/account/billing/', query_string=True)),
 ]
